8e9ecf9487c9f383e4bdebab2337ab5399ff0f2c
ValeuDoamne <alexacosmin99@gmail.com>
2024-03-25 08:10:57 +0200

    add conditional rendering to index

--- homework1/frontend/src/templates/static/js/index.js
+++ homework1/frontend/src/templates/static/js/index.js
@@ -3,3 +3,15 @@
 function login_clicked() {
     location.href = '/login';
 }
+
+function dashboard_clicked() {
+    location.href = '/dashboard';
+}
+
+window.onload = () => {
+    if (localStorage.getItem('TOKEN') !== null) {
+        const login_btn = document.getElementById('login-btn');
+        login_btn.textContent = "Dashboard >>>"
+        login_btn.onclick = dashboard_clicked;
+    }
+}
